cc5fab376374cff503df2e4f8e9e27dc1018f7c6
ST10449855 <163865994+ST10449855@users.noreply.github.com>
2024-11-07 19:33:37 +0200

    first commit

---
+++ lightbox.js
@@ -0,0 +1,28 @@
+let slideIndex = 1;
+
+function openLightbox() {
+    document.getElementById("lightboxModal").style.display = "block";
+}
+
+function closeLightbox() {
+    document.getElementById("lightboxModal").style.display = "none";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
